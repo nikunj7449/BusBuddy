425ff05f952c681b26339f580f1eaa7e1d52c3cc
nikunj <nikunjmahyavanshi24@gmail.com>
2025-12-06 23:06:27 +0530

    add frontend and backend

---
+++ Bus Booking Backend/Bus Tickets/src/main/java/com/busbuddy/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.busbuddy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5174")); // Ensure this matches your frontend
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PATCH", "PUT", "DELETE","OPTIONS"));
+        corsConfiguration.setAllowedHeaders(List.of("*")); // Allow all header
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(source);
+    }
+}
